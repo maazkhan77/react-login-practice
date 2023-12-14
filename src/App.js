7df235c76c563f1b70520d0b99550107b8048a9c
maazkhan77 <maazkhan7769@gmail.com>
2023-12-14 11:50:21 +0530

    authentication feature added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
+import Login from "./Login";
 import Register from "./Register";
 
 function App() {
   return (
     <main className="App">
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </main>
   );
 }
